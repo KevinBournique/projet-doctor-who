3e91247c1d641966d290e430d5229618827f0abf
Thomas_Pi <pijat.thomas@gmail.com>
2023-08-29 18:17:25 +0200

    add logo / modified style & ejs

--- app/database.js
+++ app/database.js
@@ -1,9 +1,10 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.PG_URL, {
-    // logging false pour ne pas polluer le terminal avec les requêtes
-    // si on veut voir les requêtes, il faut enlever cette ligne
-    // logging: false,
+    // logging => montre les requêtes dans le terminal
+    // false: ne pas voir les requêtes
+    // déclarer en fonction: voir les requêtes
+    logging: false,
     define: {
         updatedAt: 'updated_at',
         createdAt: 'created_at',
